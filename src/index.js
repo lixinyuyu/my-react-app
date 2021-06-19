955e82bfb806b8006cbcf7eed670919357cdaeb5
lixinyuyu <wb_lixinyu@kuaishou.com>
2021-06-19 18:07:05 +0800

    feat(react-app): jsx学习使用

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // import React from 'react';
 // import ReactDOM from 'react-dom';
-// import './index.css';
+import './index.css';
 // import App from './App';
 // import reportWebVitals from './reportWebVitals';
 
@@ -19,6 +19,69 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const title = React.createElement('h1', null, 'hello React !!!');
-console.log('title', title);
+// 使用jsx语法
+const title = (
+  <h1 className="title-class-name">test h1
+    <span />
+  </h1>
+);
+
+/**
+ * jsx的基本使用
+ */
+
+// 1.jsx中嵌入表达式
+const name = 'zs'
+const obj = name;
+const fn = () => obj; // 也可以直接放置一个函数
+const title2 = (
+  <h1>{name ? 'ls' : 'no'}</h1>
+)
+// 2.jsx自身也是一个合法的表达式（不能直接放置对象，会报错）
+const title3 = (
+  <h1>{fn()}</h1>
+)
+
+// 3.条件渲染
+const loadData = () => {
+  const loading = true;
+  setTimeout((loading) => {
+    loading = false;
+    console.log(loading);
+  }, 2000);
+  if (loading) {
+    return (<div>loading...</div>)
+  }
+  return (<div>end</div>)
+}
+const title4 = (
+  <div>{loadData()}</div>
+)
+
+const arrs = [
+  { label: 'label1', value: 1 },
+  { label: 'label2', value: 2 },
+  { label: 'label3', value: 3 },
+];
+// 4.jsx列表渲染
+const title5 = (
+  <ul>
+    {arrs.map(item => <li key={item.value}>{item.label}</li>)}
+  </ul>
+)
+
+// 5.jsx样式处理
+const title6 = (
+  <div style={ { color: 'red' } }>样式处理</div>
+)
+
+const title7 = (
+  <div className="color-text">样式处理</div>
+)
 ReactDOM.render(title, document.getElementById('root'));
+ReactDOM.render(title2, document.getElementById('root'));
+ReactDOM.render(title3, document.getElementById('root'));
+ReactDOM.render(title4, document.getElementById('root'));
+ReactDOM.render(title5, document.getElementById('root'));
+ReactDOM.render(title6, document.getElementById('root'));
+ReactDOM.render(title7, document.getElementById('root'));
