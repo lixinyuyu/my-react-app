0d9d82bb0c87110f8bdd4330723db6a22622a4e4
lixinyuyu <wb_lixinyu@kuaishou.com>
2021-06-30 10:57:33 +0800

    feat(react-app): 生命周期demo练习

--- src/context.js
+++ src/context.js
@@ -1,34 +1,85 @@
 import React from 'react';
-const { Provider, Consumer } = React.createContext();
+// const { Provider, Consumer } = React.createContext();
+// const MyContext = React.createContext('my---context');
+// console.log(MyContext);
+// class Parent extends React.Component {
+//     render() {
+//         return <Provider value="pink">
+//             <Child1 />
+//             {this.props.children}
+//         </Provider>
+//     }
+// }
 
-class Parent extends React.Component {
+class Text extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            temp: props.temp,
+        }
+        console.log(props, this.props, this.state);
+    }
     render() {
-        return <Provider value="pink">
-            <Child1 />
-            {this.props.children}
-        </Provider>
+        return (
+            <span>{this.state.temp}</span>
+        )
+    }
+    componentWillUnmount() {
+        console.log('componentWillUnmount');
     }
 }
 
-const Child1 = (props) => {
-    return (
-        <Child2></Child2>
-    )
-}
+// const Child1 = (props) => {
+//     return (
+//         <Child2></Child2>
+//     )
+// }
 
-const Child2 = (props) => {
-    return (
-        <Child3 />
-    )
-}
+// const Child2 = (props) => {
+//     return (
+//         <Child3 />
+//     )
+// }
 
-const Child3 = (props) => {
-    return (
-        <div>
-            <Consumer>
-                { (data) => <h1>{data}</h1>}
-            </Consumer>
-        </div>
-    )
+// const Child3 = (props) => {
+//     return (
+//         <div>
+//             <Consumer>
+//                 { (data) => <h1>{data}</h1>}
+//             </Consumer>
+//         </div>
+//     )
+// }
+
+class Parent3 extends React.Component {
+    handleClick = () => {
+        this.setState({
+            age: 100,
+        })
+    }
+    state = {
+        age: 10,
+    }
+    render() {
+        console.log('render');
+        return (
+            <div>
+                <Text temp={this.state.age}/>
+                
+                <button onClick={this.handleClick}>按钮</button>
+                {this.state.age}
+            </div>
+        )
+    }
+    componentDidMount() {
+        console.log('componentDidMount', document.querySelector('.text-span'));
+    }
+    componentDidUpdate() {
+        console.log('componentDidUpdate');
+    }
+    // shouldComponentUpdate() {
+    //     console.log('shouldComponentUpdate');
+    //     return false;
+    // }
 }
-export default Parent;
+export default Parent3;
---
+++ src/lifestyle.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Parent extends React.Component {
+    render() {
+        return this.props.children(this.state);
+    }
+    state = {
+        x: 0,
+        y: 0,
+    }
+    handleMove = e => {
+        this.setState({
+            x: e.clientX,
+            y: e.clientY,
+        });
+    }
+    componentDidMount() {
+        window.addEventListener('mousemove', this.handleMove)
+    }
+    componentWillUnmount() {
+        window.removeEventListener('mousemove');
+    }
+}
+Parent.propTypes = {
+    children: PropTypes.func.isRequired,
+}
+
+class Child extends React.Component {
+    state = {
+        x: 0,
+    }
+    render() {
+        return (
+            <div>
+                {/* <Parent render={ mouse => {
+                    return (
+                        <p>{mouse.x}-{mouse.y}</p>
+                    )
+                }}/> */}
+                <Parent>
+                    {mouse => {
+                        return <p>{mouse.x}----{mouse.y}</p>  
+                    }}
+                </Parent>
+            </div>
+        )
+    }
+}
+export default Child;
